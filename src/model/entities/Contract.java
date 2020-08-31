367044c82cad5805bedc6de1f6ad9adc442bed27
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-08-31 12:08:01 -0300

    interface without

--- src/model/entities/Contract.java
+++ src/model/entities/Contract.java
@@ -1,5 +1,67 @@
 package model.entities;
 
-public class Contract {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Contract {
+	
+	private Integer number;
+	private Double contractValue;
+	private int numberInstallment;
+	
+	private List<Installment> installment = new ArrayList<>();
+
+	public Contract(Integer number, Double contractValue, int numberInstallment, Installment installment) {
+		this.number = number;
+		this.contractValue = contractValue;
+		this.installment.add(installment);
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Double getContractValue() {
+		return contractValue;
+	}
+
+	public void setContractValue(Double contractValue) {
+		this.contractValue = contractValue;
+	}
+
+	public int getNumberInstallment() {
+		return numberInstallment;
+	}
+
+	public void setNumberInstallment(int numberInstallment) {
+		this.numberInstallment = numberInstallment;
+	}
+
+	public List<Installment> getInstallment() {
+		return installment;
+	}
+
+	public void addInstallment(Installment installment) {
+		this.installment.add(installment);
+	}
+	
+	public void removeInstallment(Installment installment) {
+		this.installment.remove(installment);
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder report = null;
+		
+		for (Installment x : installment) {
+			report.append(x.toString());
+		}
+		
+		return report.toString();
+	}
 }
--- src/model/entities/Installment.java
+++ src/model/entities/Installment.java
@@ -1,5 +1,53 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Installment {
 
+	private Date initialDate;
+	private Double amount;
+	private int plots;
+	
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	public Installment(Date initialDate, Double amount, int plots) {
+		this.initialDate = initialDate;
+		this.amount = amount;
+		this.plots = plots;
+	}
+
+	public Date getInitialDate() {
+		return initialDate;
+	}
+
+	public void setInitialDate(Date initialDate) {
+		this.initialDate = initialDate;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Double calcInstallment(int month) {
+		return amount + ((amount + (amount * 0.1) * month) * 0.2);
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder resul = null;
+		Date newDate = initialDate;
+		
+		for(int i = 1; i <= plots ;i++) {
+			newDate.setMonth(initialDate.getMonth() + i); 
+			resul.append(sdf.format(newDate) + " - " + calcInstallment(i) + "\n");
+		}
+		
+		return resul.toString();
+	}
 }
